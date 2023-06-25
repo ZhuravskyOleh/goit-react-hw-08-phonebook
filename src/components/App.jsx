2f520e96c51423ebbff5d97d62ade395e52423dc
ZhuravskyOleh <zhuravskyoleh@gmail.com>
2023-06-25 18:02:54 +0300

    add SingIn page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router";
 import { Phonebook } from "./Pages/Phonebook";
+import SignIn from "./Pages/SingIn";
 
 
 export const App = () => {
@@ -8,7 +9,9 @@ export const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/contacts" element={<Phonebook/>} />
+        <Route path="/contacts" element={<Phonebook />} />
+        <Route path="/login" element={<SignIn/>} />
+        
       </Routes>
     </div>
   );
