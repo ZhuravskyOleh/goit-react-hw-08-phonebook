ddd8ca77fb7e1217a332b10c308cc6e0f37760e7
ZhuravskyOleh <zhuravskyoleh@gmail.com>
2023-06-25 18:07:47 +0300

    add SingUp page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router";
 import { Phonebook } from "./Pages/Phonebook";
 import SignIn from "./Pages/SingIn";
+import SignUp from "./Pages/SingUp";
 
 
 export const App = () => {
@@ -11,6 +12,7 @@ export const App = () => {
       <Routes>
         <Route path="/contacts" element={<Phonebook />} />
         <Route path="/login" element={<SignIn/>} />
+        <Route path="/register" element={<SignUp/>} />
         
       </Routes>
     </div>
--- src/components/Pages/SingIn.jsx
+++ src/components/Pages/SingIn.jsx
@@ -3,8 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -89,7 +87,7 @@ export default function SignIn() {
             </Button>
             <Grid container>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/goit-react-hw-08-phonebook/register" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
