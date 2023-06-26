927c392a7b5bd7830dce0f41ec98c428ba8c677c
ZhuravskyOleh <zhuravskyoleh@gmail.com>
2023-06-26 22:23:06 +0300

    update contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,20 +1,18 @@
 import { nanoid } from '@reduxjs/toolkit';
 import Notiflix from 'notiflix';
 import { useDispatch, useSelector } from 'react-redux';
-import { Form } from './ContactForm.styled';
 import { addContact } from 'redux/contacts/operations';
 import { selectContacts } from 'redux/contacts/selectors';
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import { Container, CssBaseline, Grid, TextField, ThemeProvider, Typography, createTheme } from '@mui/material';
 
 function ContactForm() {
   const dispatch = useDispatch();
   const contacts = useSelector(selectContacts);
 
-  const handlerSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
     const name = e.target.elements.name.value;
@@ -35,31 +33,66 @@ function ContactForm() {
     dispatch(addContact(newContact));
     e.target.reset();
   };
+const defaultTheme = createTheme();
+
 
   return (
-    <Form onSubmit={handlerSubmit}>
-      <label htmlFor="name">Name </label>
-      <input
-        id="name"
-        type="text"
-        name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        required
-      />
-
-      <label htmlFor="tel">Number </label>
-      <input
-        id="tel"
-        type="tel"
-        name="number"
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-        required
-      />
-      <br />
-      <button type="submit">Add contact</button>
-    </Form>
+    <ThemeProvider theme={defaultTheme}>
+      <Typography variant="h5" textAlign={'center'}>
+        Add contact
+      </Typography>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            // marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="name"
+                  type="text"
+                  name="name"
+                  label='Name'
+                  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="tel"
+                  type="tel"
+                  name="number"
+                  label='Phone'
+                  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                  autoComplete="new-password"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Add
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
